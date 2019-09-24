60d909979cbf4802307496cab601501d3f22d167
Jabbar Mehdipour <mehdipour.py@gmail.com>
2019-09-24 12:47:11 +0330

    some change on main method

--- src/creational/builder/BuilderPatternTest.java
+++ src/creational/builder/BuilderPatternTest.java
@@ -5,7 +5,8 @@ public class BuilderPatternTest {
     public static void main(String[] args) {
         Person ava = new Person.Builder(1, "Ava", "Mehdipour").build();
 
-        System.out.println(ava.);
+        System.out.println(ava.getFirstName());
+        System.out.println(ava.getLastName());
 
     }
 
